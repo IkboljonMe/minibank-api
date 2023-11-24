738d31c3abdc4a04c5d2f372a77e57d46001aa96
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-11-24 05:33:13 +0100

    swagger configuration

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,16 @@ import express from "express";
 import logger from "./utils/logger";
 import database from "./utils/database";
 import routes from "./routes";
+import swaggerDocs from "./utils/swagger";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-app.listen(1337, async () => {
+app.listen(process.env.SERVER_PORT, async () => {
   await database();
   routes(app);
-  logger.info("Server running on http://localhost:1337");
+  logger.info(`Server running on http://localhost:${process.env.SERVER_PORT}`);
+  swaggerDocs(app, process.env.SERVER_PORT);
 });
---
+++ src/utils/swagger.ts
@@ -0,0 +1,27 @@
+import { Express, Request, Response } from "express";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { version } from "../../package.json";
+import logger from "./logger";
+
+const options: swaggerJsdoc.Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Minibank API docs",
+      version,
+    },
+  },
+  apis: ["./src/routes/**/*.ts", "./src/schemas/**/*.ts"],
+  apisSorter: "alpha",
+};
+const swaggerSpec = swaggerJsdoc(options);
+function swaggerDocs(app: Express, port: any) {
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.get("/docs.json", (req: Request, res: Response) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+  });
+  logger.info(`Docs available at http://localhost:${port}/docs`);
+}
+export default swaggerDocs;
