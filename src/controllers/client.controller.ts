db5b0dec279d69466e02f9ee333ea0f96cd2b081
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-11-24 06:23:27 +0100

    client routes swagger finished

--- src/controllers/client.controller.ts
+++ src/controllers/client.controller.ts
@@ -4,26 +4,47 @@ import { PostgresData } from "../utils/database";
 
 export async function createClientHandler(req: Request, res: Response) {
   const { firstName, lastName, email, cardNumber, balance } = req.body;
-  const client = await PostgresData.createQueryBuilder()
-    .insert()
-    .into(Client)
-    .values({
-      first_name: firstName,
-      last_name: lastName,
-      email,
-      card_number: cardNumber,
-      balance,
-    })
-    .execute();
-  if (!client) {
-    return res.sendStatus(404);
+
+  try {
+    const result = await PostgresData.createQueryBuilder()
+      .insert()
+      .into(Client)
+      .values({
+        first_name: firstName,
+        last_name: lastName,
+        email,
+        card_number: cardNumber,
+        balance,
+      })
+      .execute();
+
+    const createdClient = result.generatedMaps[0] as Client;
+
+    return res.status(200).json({
+      message: "Client created successfully",
+      client: createdClient,
+    });
+  } catch (error) {
+    console.error("Error creating client:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
-  return res.json(client);
 }
+
 export async function deleteClientHandler(req: Request, res: Response) {
   const { clientId } = req.params;
 
-  const response = await Client.delete(clientId);
+  try {
+    const deleteResult = await Client.delete(clientId);
 
-  return res.json(response);
+    if (deleteResult.affected === 0) {
+      return res.status(404).json({ error: "Client not found" });
+    }
+
+    return res.status(200).json({
+      message: "Client deleted successfully",
+    });
+  } catch (error) {
+    console.error("Error deleting client:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 }
--- src/routes/client.routes.ts
+++ src/routes/client.routes.ts
@@ -3,8 +3,101 @@ import {
   createClientHandler,
   deleteClientHandler,
 } from "../controllers/client.controller";
+
 function clientRoutes(app: Express) {
   app.post("/api/client", createClientHandler);
   app.delete("/api/client/:clientId", deleteClientHandler);
 }
 export default clientRoutes;
+
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     CreateClientInput:
+ *       type: object
+ *       required:
+ *         - firstName
+ *         - lastName
+ *         - email
+ *         - cardNumber
+ *         - balance
+ *       properties:
+ *         firstName:
+ *           type: string
+ *           default: clienttest
+ *         lastName:
+ *           type: string
+ *           default: clienttest
+ *         email:
+ *           type: string
+ *           default: clienttest@gmail.com
+ *         cardNumber:
+ *           type: string
+ *           default: 12121212
+ *         balance:
+ *           type: number
+ *           default: 10000
+ */
+/**
+ * @openapi
+ * /api/client:
+ *   post:
+ *     tags:
+ *       - Create a client
+ *     summary: Create a new client
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CreateClientInput'
+ *     responses:
+ *       '200':
+ *         description: Client created successfully
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: Client created successfully
+ *               client:
+ *                 id: 1
+ *                 first_name: clienttest
+ *                 last_name: clienttest
+ *                 email: clienttest@gmail.com
+ *                 card_number: null
+ *                 balance: null
+ *       '404':
+ *         description: Not found
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Internal Server Error
+ */
+/**
+ * @openapi
+ * /api/client/{clientId}:
+ *   delete:
+ *     tags:
+ *       - Delete a client
+ *     summary: Delete a client by ID
+ *     parameters:
+ *       - in: path
+ *         name: clientId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the client to delete
+ *     responses:
+ *       '200':
+ *         description: Client deleted successfully
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: Client deleted successfully
+ *       '404':
+ *         description: Not found
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Client not found
+ */
